932697e98b9a7f282b596627989773d4d4ebc17c
micpuc157_dpp_2023 <micpuc157_dpp_2023@e-science.pl>
2023-07-26 21:42:17 +0200

    done

--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,17 +1,86 @@
 package com.nphase.service;
 
+import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShoppingCartService {
 
+    private int bulkItemThreshold; // The number of items required to get the bulk discount
+    private double bulkItemDiscount; // The percentage discount for bulk items
+
+    // Map to store the discount policy for each category
+    private Map<String, DiscountPolicy> categoryDiscountPolicies;
+
+    public ShoppingCartService(int bulkItemThreshold, double bulkItemDiscount) {
+        this.bulkItemThreshold = bulkItemThreshold;
+        this.bulkItemDiscount = bulkItemDiscount;
+        this.categoryDiscountPolicies = new HashMap<>();
+    }
+
+    // Add a discount policy for a specific category
+    public void addCategoryDiscountPolicy(String category, int itemThreshold, double discountPercentage) {
+        DiscountPolicy discountPolicy = new DiscountPolicy(itemThreshold, discountPercentage);
+        categoryDiscountPolicies.put(category, discountPolicy);
+    }
+
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
-        return shoppingCart.getProducts()
-                .stream()
-                .map(product -> product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity())))
-                .reduce(BigDecimal::add)
-                .orElse(BigDecimal.ZERO);
+        BigDecimal total = BigDecimal.ZERO;
+
+        for (Product product : shoppingCart.getProducts()) {
+            int quantity = product.getQuantity();
+            BigDecimal pricePerUnit = product.getPricePerUnit();
+            BigDecimal productTotal;
+
+            productTotal = pricePerUnit.multiply(BigDecimal.valueOf(quantity));
+
+            // Check if there's a discount policy for the product's category
+            if (product.getCategory() != null && categoryDiscountPolicies.containsKey(product.getCategory())) {
+                DiscountPolicy discountPolicy = categoryDiscountPolicies.get(product.getCategory());
+                productTotal = productTotal.subtract(calculateProductDiscount(quantity, pricePerUnit, discountPolicy));
+            }
+
+            // Check if there's a bulk dicscount
+            productTotal = productTotal.subtract(calculateBulkDiscount(quantity, pricePerUnit));
+            total = total.add(productTotal);
+        }
+
+        return total;
+    }
+
+    private BigDecimal calculateProductDiscount(int quantity, BigDecimal pricePerUnit, DiscountPolicy discountPolicy) {
+        int discountedQuantity = quantity;
+        if (quantity > discountPolicy.itemThreshold) {
+            // Calculate the number of items to apply the discount to
+            discountedQuantity = quantity;
+            //- (quantity / discountPolicy.itemThreshold);
+
+        }
+
+        // Calculate the product total after applying the discount
+        BigDecimal discountedPricePerUnit = pricePerUnit.multiply(BigDecimal.valueOf(discountPolicy.discountPercentage));
+
+        return discountedPricePerUnit.multiply(BigDecimal.valueOf(discountedQuantity));
+    }
+
+    private BigDecimal calculateBulkDiscount(int quantity, BigDecimal pricePerUnit) {
+        if (quantity > bulkItemThreshold) {
+            BigDecimal discountedPricePerUnit = pricePerUnit.multiply(BigDecimal.valueOf(bulkItemDiscount));
+            return discountedPricePerUnit.multiply(BigDecimal.valueOf(quantity));
+        }
+        return BigDecimal.ZERO;
+    }
+
+    private static class DiscountPolicy {
+        private final int itemThreshold;
+        private final double discountPercentage;
+
+        public DiscountPolicy(int itemThreshold, double discountPercentage) {
+            this.itemThreshold = itemThreshold;
+            this.discountPercentage = discountPercentage;
+        }
     }
 }
--- src/test/java/com/nphase/service/ShoppingCartServiceTest.java
+++ src/test/java/com/nphase/service/ShoppingCartServiceTest.java
@@ -1,28 +1,80 @@
 package com.nphase.service;
 
-
 import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
+import com.nphase.service.ShoppingCartService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class ShoppingCartServiceTest {
-    private final ShoppingCartService service = new ShoppingCartService();
 
+    private ShoppingCartService service;
+
+    @BeforeEach
+    public void setup() {
+        service = new ShoppingCartService(3, 0.10);
+        service.addCategoryDiscountPolicy("drinks", 3, 0.10);
+    }
     @Test
-    public void calculatesPrice()  {
+    public void calculateTotalPriceWithoutDiscount() {
         ShoppingCart cart = new ShoppingCart(Arrays.asList(
-                new Product("Tea", BigDecimal.valueOf(5.0), 2),
-                new Product("Coffee", BigDecimal.valueOf(6.5), 1)
+                new Product("Tea", BigDecimal.valueOf(5.0), 2, null),
+                new Product("Coffee", BigDecimal.valueOf(6.5), 1, null)
         ));
 
         BigDecimal result = service.calculateTotalPrice(cart);
 
-        Assertions.assertEquals(result, BigDecimal.valueOf(16.5));
+        Assertions.assertEquals(BigDecimal.valueOf(16.5), result);
     }
 
+    @Test
+    public void calculateTotalPriceWithBulkDiscount() {
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(
+                new Product("Tea", BigDecimal.valueOf(5.0), 5, null),
+                new Product("Coffee", BigDecimal.valueOf(3.5), 3, null)
+        ));
+
+        BigDecimal result = service.calculateTotalPrice(cart);
+
+        // 10% discount for 5 Tea items (5 * 5.0 * 0.9) + 10.5 (3 * 3.5)
+        BigDecimal expected = BigDecimal.valueOf(22.5).add(BigDecimal.valueOf(10.5));
+
+        // Use stripTrailingZeros() to remove any trailing zeros for comparison
+        Assertions.assertEquals(expected.stripTrailingZeros(), result.stripTrailingZeros());
+    }
+
+    @Test
+    public void calculateTotalPriceWithCategoryDiscount() {
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(
+                new Product("Tea", BigDecimal.valueOf(5.3), 2, "drinks"),
+                new Product("Coffee", BigDecimal.valueOf(3.5), 2, "drinks"),
+                new Product("Cheese", BigDecimal.valueOf(8), 2, "food")
+        ));
+
+        BigDecimal result = service.calculateTotalPrice(cart);
+
+        // 10% discount for 4 drinks (2 * 5.3 * 0.9) + 6.3 (2 * 3.5 * 0.9) + 16 (2 * 8)
+        Assertions.assertEquals(BigDecimal.valueOf(9.54).add(BigDecimal.valueOf(6.3)).add(BigDecimal.valueOf(16)), result);
+    }
+
+    @Test
+    public void calculateTotalPriceWithBulkAndCategoryDiscount() {
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(
+                new Product("Tea", BigDecimal.valueOf(5.3), 5, "drinks"),
+                new Product("Coffee", BigDecimal.valueOf(3.5), 2, "drinks"),
+                new Product("Cheese", BigDecimal.valueOf(8), 2, "food")
+        ));
+
+        BigDecimal result = service.calculateTotalPrice(cart);
+
+        // 10% discount for 7 drinks, 10% discount for 5 Tea items
+        BigDecimal expected = BigDecimal.valueOf(21.2).add(BigDecimal.valueOf(6.3)).add(BigDecimal.valueOf(16));
+
+        // Use stripTrailingZeros() to remove any trailing zeros for comparison
+        Assertions.assertEquals(expected.stripTrailingZeros(), result.stripTrailingZeros());
+    }
 }
